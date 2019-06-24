68cc468ffd2b44bd1e5cd468bc56350af19263ae
y_uuki <yuki.tsubo@gmail.com>
2019-06-24 15:35:15 +0900

    Add test InsertOrUpdateHostFlows

--- db/db_test.go
+++ db/db_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/yuuki/lstf/tcpflow"
 )
 
 func TestCreateSchema(t *testing.T) {
@@ -13,6 +14,28 @@ func TestCreateSchema(t *testing.T) {
 	mock.ExpectExec("CREATE TYPE (.+)").WillReturnResult(sqlmock.NewResult(1, 1))
 
 	err := db.CreateSchema()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestInsertOrUpdateHostFlows(t *testing.T) {
+	db, mock := NewTestDB()
+	defer db.Close()
+
+	flows := tcpflow.HostFlows{}
+
+	mock.ExpectBegin()
+	mock.ExpectPrepare("INSERT INTO nodes")
+	mock.ExpectPrepare("SELECT node_id FROM nodes")
+	mock.ExpectPrepare("INSERT INTO flows")
+	mock.ExpectCommit()
+
+	err := db.InsertOrUpdateHostFlows(flows)
 	if err != nil {
 		t.Fatal(err)
 	}
