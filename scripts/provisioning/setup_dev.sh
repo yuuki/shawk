909f476dc6ab071eb1fa8d97997c3b1b5e5326d6
y_uuki <yuki.tsubo@gmail.com>
2020-04-07 09:53:56 +0900

    Install postgres to VM

--- scripts/provisioning/setup_dev.sh
+++ scripts/provisioning/setup_dev.sh
@@ -16,4 +16,11 @@ echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 
 mkdir -p /home/vagrant/go/src/github.com/yuuki/shawk/
 
+# Install postgres
+PG_VERSION=11
+
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo bash -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+sudo apt-get -yq update
+sudo apt-get install -yq postgresql-${PG_VERSION} postgresql-client-${PG_VERSION} postgresql-contrib-${PG_VERSION}
 echo 'Completed to setup'
